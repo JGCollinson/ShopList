44e9e845be417d2c2ddcfa79a977301b2f6a9b73
JGCollinson <jgarrettcollinson@outlook.com>
2018-03-29 19:14:06 -0400

    Update shoplist.js

--- shoplist.js
+++ shoplist.js
@@ -1,3 +1,8 @@
+TESTER = document.getElementById('tester');
+Plotly.plot( TESTER, [{
+x: [1, 2, 3, 4, 5],
+y: [1, 2, 4, 8, 16] }], {
+margin: { t: 0 } } );
 
 var myList = []; 
 $(document).ready(function(){
