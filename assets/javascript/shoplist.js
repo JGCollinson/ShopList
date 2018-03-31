b3da4895a044a95369db84955a4a5d338a5e5830
Your Name <yaotaylor25@gmail.com>
2018-03-31 16:09:49 -0400

    front: p1

---
+++ assets/javascript/shoplist.js
@@ -0,0 +1,75 @@
+
+
+
+var myList = [];
+$(document).ready(function(){
+
+    var data = [{
+        values: [19, 26, 55],
+        labels: ['Residential', 'Non-Residential', 'Utility'],
+        type: 'pie'
+      }];
+      
+//---------------PLOTLY-----------------------
+      Plotly.newPlot('tester', data);
+//--------------------------------------------  
+    
+    $("#addButtons").on("click", function(){ 
+        event.preventDefault();
+        var inputVal = $("#addNumber").val().trim();
+        myList.push(inputVal);
+        console.log(myList);
+        $(".userList").empty()
+        for(var i=0; i<myList.length; i++){
+            $("#addNumber").val("");
+            $(".userList").append(`
+               <div class="row">
+                <img class="avaibility" id="redArrow" src="https://storage.googleapis.com/coding-bc-projects/ShopList/redArrow.jpg" alt="redArrow">
+                <h6 class="itemNames"></h6>
+                <button class= "numbers">${myList[i]}</button>
+                <h6 class="listCategory"></h6>       
+                
+               </div>
+           `);
+        
+            $(".numbers").css({
+            "border": "3px solid yellow", 
+            "background-color": "blue", 
+            "color": "white",
+            "width": "50px",
+            "text-align": "center",
+            "height": "40px",
+            "margin-top": "5px"
+            });
+
+            $(".itemNames").css({
+            "border": "3px solid yellow", 
+            "background-color": "blue", 
+            "color": "white",
+            "width": "180px",
+            "text-align": "center",
+            "height": "40px",
+            "margin-top": "5px",
+            "margin-right": "10px"
+            });
+
+            $(".listCategory").css({
+            "border": "3px solid yellow", 
+            "background-color": "blue", 
+            "color": "white",
+            "width": "180px",
+            "text-align": "center",
+            "height": "40px",
+            "margin-top": "5px",
+            "margin-left": "10px"
+            
+            });
+        
+           
+        }
+    });
+
+
+});
+
+
