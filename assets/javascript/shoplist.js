b492f41c3e1821af4976dd4e6b250e000e981257
Garrett Collinson <jgarrettcollinson@outlook.com>
2018-04-02 20:44:39 -0400

    added new api logic

--- assets/javascript/shoplist.js
+++ assets/javascript/shoplist.js
@@ -3,17 +3,16 @@
 
 var myList = [];
 $(document).ready(function(){
-
+    
     var data = [{
         values: [19, 26, 55],
         labels: ['Residential', 'Non-Residential', 'Utility'],
         type: 'pie'
-      }];
-      
-//---------------PLOTLY-----------------------
-      Plotly.newPlot('tester', data);
-//--------------------------------------------  
+    }];
     
+    //---------------PLOTLY-----------------------
+    Plotly.newPlot('tester', data);
+    //--------------------------------------------  
     $("#addButtons").on("click", function(){ 
         event.preventDefault();
         var inputVal = $("#addNumber").val().trim();
@@ -41,7 +40,7 @@ $(document).ready(function(){
             "height": "40px",
             "margin-top": "5px"
             });
-
+    
             $(".itemNames").css({
             "border": "3px solid yellow", 
             "background-color": "blue", 
@@ -52,7 +51,7 @@ $(document).ready(function(){
             "margin-top": "5px",
             "margin-right": "10px"
             });
-
+    
             $(".listCategory").css({
             "border": "3px solid yellow", 
             "background-color": "blue", 
@@ -62,14 +61,92 @@ $(document).ready(function(){
             "height": "40px",
             "margin-top": "5px",
             "margin-left": "10px"
-            
-            });
-        
-           
+            });  
         }
     });
+    });
+    
+function productInfo() {
+    var itemName = "tomato"
+    var itemVal = "976759"
+    var queryURL =
+        "https://cors-anywhere.herokuapp.com/http://api.walmartlabs.com/v1/search?query=" +
+        itemName +
+        "&format=json&categoryId=" +
+        itemVal +"&apiKey=x5k7prwzkqgurwt4n33rt74g";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function (response) {
+        var results = response.items;
+        console.log(results[0].salePrice);
+        console.log(results[0].thumbnailImage);
+        console.log(results[0].productUrl);
+        console.log(results[0].offerType);
+    })
+};
 
+var latitued;
+var longitude;
 
-});
+function latLongLookup() {
+    var zipCode = "30309"
+    var queryURL =
+        "https://cors-anywhere.herokuapp.com/https://www.zipcodeapi.com/rest/ycSGL95P7qvqRje1BBye5ASSV4LaYIrbGOJzDF1yP6Me5yQmG9YGPneweDWslVM5/info.json/" +
+        zipCode +
+        "degrees";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function (response) {
+        latitude = Number(response.lat);
+        longitude  = Number(response.lng);
+        console.log(latitude);
+        console.log(longitude);
 
+      })
+    };
 
+    productInfo();
+    latLongLookup();
+    
+    var map;
+    var infowindow;
+    
+    function initMap() {
+      var pyrmont = {lat: -33.867, lng: 151.195};
+    
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: pyrmont,
+        zoom: 15
+      });
+    
+      infowindow = new google.maps.InfoWindow();
+      var service = new google.maps.places.PlacesService(map);
+      service.nearbySearch({
+        location: pyrmont,
+        radius: 500,
+        type: ['store']
+      }, callback);
+    };
+    
+    function callback(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+        }
+      }
+    };
+    
+    function createMarker(place) {
+      var placeLoc = place.geometry.location;
+      var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+      });
+    
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+      });
+    };
